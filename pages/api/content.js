a41f92591666000a4fb9bd3478673abe0a9d8a1c
littlecm <clittle.norml@gmail.com>
2024-05-09 19:02:46 -0400

    Update content.js

--- pages/api/content.js
+++ pages/api/content.js
@@ -1,25 +1,6 @@
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 
-export default async function handler(req, res) {
-    try {
-        // Path to your HTML file within the Next.js project
-        const filePath = join(process.cwd(), 'public', 'corvettetest.html');
-
-        // Read the content of the HTML file
-        const htmlContent = await readFile(filePath, 'utf8');
-
-        // Set the content type to 'text/html'
-        res.setHeader('Content-Type', 'text/html');
-
-        // Send the HTML content
-        res.status(200).send(htmlContent);
-    } catch (error) {
-        // Handle any errors that occur during file reading
-        res.status(500).send('Error loading the page');
-    }
-}
-
 
 export default async function handler(req, res) {
     // Set CORS headers
